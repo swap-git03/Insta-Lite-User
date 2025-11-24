fd8a10051d825b9eab982f09613f7d0a830bef13
swap-git03 <swapnilfulpagare061003@gmail.com>
2025-11-24 18:38:37 +0530

    /

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://swap-insta-backend.onrender.com/api",
+baseURL: "https://swap-insta-backend.onrender.com",
 });
 
 // Automatically attach token
